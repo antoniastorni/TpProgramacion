67c28afd358f66a675ef7f83391907ef787da32d
juancruzestevez <juan.estevez@ing.austral.edu.ar>
2020-10-12 22:36:11 -0300

    prueba

--- src/Archivios.java
+++ src/Archivios.java
@@ -6,7 +6,7 @@ public class Archivios {
     public static void main(String[] args) {
        String line;
         List<Ciudadano> ciudadanos = new ArrayList<Ciudadano>();
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("/media/rodrigo/Datos/Austral/Facultad/Compu2/Archivo_texto/src/Covid/datos.txt"));){
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("archivo.txt"));){
             while ((line = bufferedReader.readLine()) != null) {
                 String[] ciudadano = line.split(",");
                 ciudadanos.add(new Ciudadano(Integer.parseInt(ciudadano[0]),Integer.parseInt(ciudadano[1]),(ciudadano[2])));
