6ce57125b3675ac3669093d79b7ab79d03de4fdf
juancruzestevez <juan.estevez@ing.austral.edu.ar>
2020-10-12 22:56:09 -0300

    prueba

--- src/Archivios.java
+++ src/Archivios.java
@@ -6,7 +6,7 @@ public class Archivios {
     public static void main(String[] args) {
        String line;
         List<Ciudadano> ciudadanos = new ArrayList<Ciudadano>();
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("archivo.txt"));){
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("C:/Users/Usuario/IdeaProjects/TpProgramacion/src/datos.txt"));){
             while ((line = bufferedReader.readLine()) != null) {
                 String[] ciudadano = line.split(",");
                 ciudadanos.add(new Ciudadano(Integer.parseInt(ciudadano[0]),Integer.parseInt(ciudadano[1]),(ciudadano[2])));
